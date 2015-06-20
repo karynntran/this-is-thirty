b2adecc74b57e12c4757a903b79f82039ffdb69b
karynntran <karynn.tran@gmail.com>
2015-06-20 17:53:40 -0400

    Added default marker position

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,7 +13,18 @@ var months = [
 	"december"
 ]
 
+var defaultPosition = function(){
+	var defaultMonth = document.getElementById('januaryNav');
+	var marker = document.getElementById('marker');
+	var monthWidth = marker.offsetWidth/4;
+	var monthLeftPosition = marker.offsetLeft;
+
+	marker.style.left=parseFloat(monthLeftPosition) + parseFloat(monthWidth) + 'px';
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
+
+	defaultPosition();
 
 	for (var i=0; i < months.length; i++){
 		var month = document.getElementById(months[i] + 'Nav');
