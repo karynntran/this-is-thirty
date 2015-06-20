b592ce7568a6394d812d1cf4ec035508dc2bbcdc
karynntran <karynn.tran@gmail.com>
2015-06-20 13:26:47 -0400

    Adjustments for app renaming

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nyc-todo-see');
+var db = monk('localhost:27017/this-is-thirty');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
