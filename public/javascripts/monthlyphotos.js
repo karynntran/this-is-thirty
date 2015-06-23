8e7af37785345239549442e45e7e73cab94a98cf
karynntran <karynn.tran@gmail.com>
2015-06-23 14:55:18 -0400

    Wrangling a config file

--- public/javascripts/monthlyphotos.js
+++ public/javascripts/monthlyphotos.js
@@ -1,3 +1,90 @@
+var navigation = [
+	{
+	    "month":"home"
+
+  	},
+	{
+	    "month":"january",
+	    "images": 
+	    	[
+	    		{'image':'01janBirthday.jpg', 'description': 'My 30th Birthday'},
+	    		{'image':'01janBirthday2.jpg', 'description': 'More Birthday'}
+	    	]
+  	},
+	{
+	    "month":"february",
+    	"images": 
+	    	['02febKatrinaBabyShower.jpg',
+	    	'02febLunarNewYear.jpg',
+	    	'02febMauBabyShower',
+	    	'02febStartTD.jpg',
+	    	'02febValentines.jpg',
+	    	'02febValentines2.jpg'
+	    	]
+  	},
+	{
+	    "month":"march",
+    	"images": 
+	    	['03marAlexPamie.jpg',
+	    	'03marBensBirthday.jpg',
+	    	'03marBensBirthday2.jpg',
+	    	'03marGalTech.jpg',
+	    	'03marLearningNode.jpg',
+	    	'03marNicoBorn.jpg',
+	    	'03marStPete.jpg'
+	    	]
+  	},
+	{
+	    "month":"april"
+  	},
+	{
+	    "month":"may"
+  	},
+	{
+	    "month":"june"
+  	},
+	{
+	    "month":"july"
+  	},
+	{
+	    "month":"august"
+  	},
+	{
+	    "month":"september"
+  	},
+	{
+	    "month":"october"
+  	},
+	{
+	    "month":"november"
+  	},
+	{
+	    "month":"december"
+  	}
+]
+
+defaultPosition = function () {
+    var defaultMonth = document.getElementById('januaryNav');
+	var marker = document.getElementById('marker');
+	var monthWidth = marker.offsetWidth/4;
+	var monthLeftPosition = marker.offsetLeft;
+
+	marker.style.left=parseFloat(monthLeftPosition) + parseFloat(monthWidth) + 'px';
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
-	console.log("monthlyphotos");
+
+	for (var i = 0; i < navigation.length; i++){
+		var month = document.getElementById(navigation[i].month + 'Nav');
+
+		month.onclick = (function(){
+			return function(){
+				var marker = document.getElementById('marker');
+				var monthWidth = this.offsetWidth/4;
+				var monthLeftPosition = this.offsetLeft;
+
+				marker.style.left=parseFloat(monthLeftPosition) + parseFloat(monthWidth) + 'px';
+			};
+		})();
+	};
 });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var file = __dirname + '/object.json';
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) {
+    console.log('Error: ' + err);
+    return;
+  }
+  jsonData = JSON.parse(data);
+  console.dir(data);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +18,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/monthlyphotos', function(req, res, next) {
   res.render('monthlyphotos', {scripts: '../javascripts/monthlyphotos.js'});
+  // res.render('monthlyphotos', {jsonData: '../javascripts/object.js'});
 });
 
 /* GET bucketlisttest list */
